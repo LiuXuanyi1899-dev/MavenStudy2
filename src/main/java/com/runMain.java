378427826e2483429ef8b482ae1b9bf348b1bd8b
LiuXuanyi1899-dev <lxy18996773375@outlook.com>
2022-11-16 12:43:54 +0800

    hot-fix first commit

--- src/main/java/com/runMain.java
+++ src/main/java/com/runMain.java
@@ -4,5 +4,6 @@ public class runMain {
     public static void main(String[] args) {
         System.out.println("HI");
         System.out.println("HI2");
+        System.out.println("HI3");
     }
 }
